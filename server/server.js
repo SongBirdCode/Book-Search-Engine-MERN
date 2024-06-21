4d0a7758e072e80efad669d0b031d1902aab8edd
SongBirdCode <Songbirdcode@gmail.com>
2024-06-20 20:25:35 -0400

    set up Apollo server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { ApolloServer } = require('apollo-server-express')
+// const { typeDefs, resolvers } = require()
+// const { authMiddleware } = require
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,8 +18,23 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+app.get('/', (req, res) => {
+  res.sendfile(path.join(__dirname, '../client/build/index.html'));
+})
+
+// Apollo server instance with GraphQL schema added
+const startApolloserver = async (TypeDefs, resolvers) => (
+  await server.start();
+  server.applyMiddleware({ app });
+)
+
+// indicate PORT(S) 
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  console.log('Use GraphQL @ http://localhost:${PORT}${server.graphqlPath}');
+  });
+
+  // async function call in order to start the server
+  startApolloserver(TypeDefs, resolvers);
